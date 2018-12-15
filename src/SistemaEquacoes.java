55f22e5fa0d424a1197ca1bb8d299832f1b645c1
William Maejima <william.maejima@gmail.com>
2018-12-15 14:56:05 -0200

    finalizado

--- src/SistemaEquacoes.java
+++ src/SistemaEquacoes.java
@@ -7,5 +7,94 @@ public class SistemaEquacoes {
 		this.matrizA=matrizA;
 		this.matrizB=matrizB;
 	}
-
+	
+	public double[] calcularSistema() {
+		double[][] matrizInv = matrizInversa(matrizA);
+		double[] matrizX = new double[matrizA.length];
+		for(int i=0;i<matrizA.length;i++) {
+			matrizX[i]=0;	
+			for(int j=0;j<matrizA.length;j++) {
+				matrizX[i]+=matrizInv[i][j]*matrizB[j];
+			}
+		}
+		return matrizX;
+	}
+	
+	public double[][] matrizInversa(double[][] matriz){
+		double[][] matrizInv = new double[matriz.length][matriz.length];
+		double[][] matrizCofat = matrizCofatores(matriz);
+		double[][] matrizTrans = matrizTransposta(matrizCofat);
+		double det = determinante(matriz);
+		for(int i=0;i<matriz.length;i++)
+			for(int j=0;j<matriz.length;j++)
+				matrizInv[i][j]=1/det*matrizTrans[i][j];
+		return matrizInv;
+	}
+	
+	public double[][] matrizCofatores(double[][] matriz){
+		double[][] matrizCofat = new double[matriz.length][matriz.length];
+		for(int i=0;i<matriz.length;i++)
+			for(int j=0;j<matriz.length;j++)
+				matrizCofat[i][j]=Math.pow(-1, i+j)*determinante(matrizResultante(matriz,i,j));
+		return matrizCofat;
+	}
+	
+	public double[][] matrizResultante(double[][] matriz,int i, int j){
+		double[][] matrizRes = new double[matriz.length-1][matriz.length-1];
+		int linha=0;
+		for(int k=0;k<matriz.length;k++) {
+			if(k==i)
+				continue;
+			else {
+				int coluna=0;
+				for(int l=0;l<matriz.length;l++) {
+					if(l==j)
+						continue;
+					else {
+						matrizRes[linha][coluna]=matriz[k][l];
+						coluna++;
+					}
+				}
+				linha++;
+			}
+		}
+		return matrizRes;
+	}
+	
+	public double[][] matrizTransposta(double[][] matriz){
+		double[][] matrizTrans = new double[matriz.length][matriz.length];
+		for(int i=0;i<matriz.length;i++)
+			for(int j=0;j<matriz[i].length;j++)
+				matrizTrans[j][i] = matriz[i][j];
+		return matrizTrans;
+	}
+	
+	public double determinante(double[][] matriz) {
+		if(matriz.length==1)
+			return matriz[0][0];
+		else if(matriz.length==2)
+			return matriz[0][0]*matriz[1][1]-matriz[0][1]*matriz[1][0];
+		else if(matriz.length==3)
+			return matriz[0][0]*matriz[1][1]*matriz[2][2]+
+					matriz[0][1]*matriz[1][2]*matriz[2][0]+
+					matriz[1][0]*matriz[2][1]*matriz[0][2]-
+					matriz[0][2]*matriz[1][1]*matriz[2][0]-
+					matriz[0][0]*matriz[1][2]*matriz[2][1]-
+					matriz[0][1]*matriz[1][0]*matriz[2][2];
+		else
+		{
+			double det=0;
+			for(int i=0;i<matriz.length;i++) {
+				int j=0;
+				double[][] matrizRes = matrizResultante(matriz,i,j);
+				det+=matriz[i][j]*Math.pow(-1, i+j)*determinante(matrizRes);
+			}
+			return det;
+			
+		}
+			
+	}
+	
+	
+	
 }
--- src/inicio.java
+++ src/inicio.java
@@ -3,20 +3,41 @@ public class inicio {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int dimMatriz = 2;
+		int dimMatriz = 4;
 		double[][] matrizA = new double[dimMatriz][dimMatriz];
 		double[] matrizB = new double[dimMatriz];
 		double[] matrizX = new double[dimMatriz];
 		
 		matrizA[0][0]=2;
 		matrizA[0][1]=3;
+		matrizA[0][2]=5;
+		matrizA[0][3]=6;
+		
 		matrizA[1][0]=4;
 		matrizA[1][1]=2;
+		matrizA[1][2]=1;
+		matrizA[1][3]=1;
+		
+		matrizA[2][0]=5;
+		matrizA[2][1]=1;
+		matrizA[2][2]=2;
+		matrizA[2][3]=3;
+		
+		matrizA[3][0]=6;
+		matrizA[3][1]=1;
+		matrizA[3][2]=3;
+		matrizA[3][3]=2;
 		
 		matrizB[0]=2;
 		matrizB[1]=4;
+		matrizB[2]=6;
+		matrizB[3]=10;
 		
-		SistemaEquacoes eqs = new SistemaEquacoes(matrizA,matrizB);	
+		SistemaEquacoes eqs = new SistemaEquacoes(matrizA,matrizB);
+		matrizX=eqs.calcularSistema();
+		for(int i=0;i<matrizX.length;i++)
+			System.out.println(matrizX[i]);
+			
 		
 
 	}
